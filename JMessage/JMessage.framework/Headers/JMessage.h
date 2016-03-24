500d04acc60605e95e9abfddca6da79d0b429231
HuangMin <huangmin@jpush.cn>
2016-03-24 15:35:20 +0800

    update jmessage 2.0.1

--- JMessage/JMessage.framework/Headers/JMessage.h
+++ JMessage/JMessage.framework/Headers/JMessage.h
@@ -38,10 +38,10 @@
 @interface JMessage : NSObject
 
 /*! JMessage SDK 版本号。用于展示 SDK 的版本信息 */
-#define JMESSAGE_VERSION @"2.0.0"
+#define JMESSAGE_VERSION @"2.0.1"
 
 /*! JMessage SDK 构建ID. 每次构建都会增加 */
-#define JMESSAGE_BUILD 1064
+#define JMESSAGE_BUILD 1082
 
 /*! API Version - int for program logic. SDK API 有变更时会增加 */
 extern NSInteger const JMESSAGE_API_VERSION;
